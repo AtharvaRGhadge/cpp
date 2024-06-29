1ee14bd8b09048f9b025141b4a87fc08c5f110d6
Atharva Ravindra Ghadge <raghadge1979@gmail.com>
2024-06-29 13:19:10 +0530

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//program to swap two numbers with using a temporary variable
+int main(){
+    int a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    cout<<" 1st number is "<<a<<"\n";
+    cout<<" 2nd number is "<<b<<"\n";
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"after swapping, 1st number is "<<a<<"\n";
+    cout<<"after swapping, 2nd number is "<<b<<"\n";
+}
