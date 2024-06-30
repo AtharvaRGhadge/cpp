22ebb9a245d4684000e76800bfdcfeec50789e63
Atharva Ravindra Ghadge <raghadge1979@gmail.com>
2024-06-30 11:40:41 +0530

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+using namespace std;
+//Check whether input character is vowel or consonant
+int main(){
+    char x;
+    cout<<"enter a character";
+    cin>>x;
+    if (x=='a'|| x=='A'){
+        cout<<x<<" is a vowel";
+    }
+    else if(x=='e'|| x=='E'){
+        cout<<x<<" is a vowel";
+    }
+    else if(x=='i' || x=='I'){
+        cout<<x<<" is a vowel";
+    }
+    else if(x=='o' || x=='O'){
+        cout<<x<<" is a vowel";
+    }
+    else if(x=='u' || x=='U'){
+        cout<<x<<" is a vowel";
+    }
+    else{
+        cout<<x<<" is a consonant";
+    }
+}    
+
+
